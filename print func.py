5b40cfd55e424c2671913294f68cadb7de7382a4
CseKiran <kvohad@gmail.com>
2024-08-05 16:17:54 +0530

    Add files via upload

---
+++ print func.py
@@ -0,0 +1,4 @@
+print("Hello world")#double quotes
+print('Hello world')#single quotes
+print("Hello 'kiran' world")#single quotes  to use in duoble quotes 
+print('Hello "kiran" world')#duoble quotes to use in single  
