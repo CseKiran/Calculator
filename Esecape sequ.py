b595ccf014e860240ee7d493f1f22792baf85780
CseKiran <kvohad@gmail.com>
2024-08-05 16:17:10 +0530

    Add files via upload

---
+++ Esecape sequ.py
@@ -0,0 +1,30 @@
+#Escape sequence---->
+
+# \' = single quote(')
+print(' I\'am kiran')
+#\" = duoble quote(")
+print("I\"am vohad")
+#\\ = backslash(\)
+print("I \\ am kiran")
+#\n = use to in move next line 
+print(" my name is kiran\nmy surname is vohad")
+#\t  = use to in tab
+print("name\tsurname")
+#\b = use to in silent character
+print("hell\blo") 
+print("lineA\\nlineB")
+print("\\\"\\\'")
+
+#Exercise1
+
+#this is \\ duoble backslash 
+#there are /\/\/\/\/\ mountion 
+#he is   owesome
+# \'\n\t\"
+
+#solution
+
+print("this is \\\\ double backslash")
+print("there are /\\/\\/\\/\\ mountion")
+print("he is\towsome")
+print("\\'\\n\\t\\'")
